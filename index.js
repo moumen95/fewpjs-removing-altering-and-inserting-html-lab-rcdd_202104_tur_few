89a65d15723f2ac552aefee9277068dbe9d94a80
Moumen Hallak <moumen95@gmail.com>
2021-04-23 14:07:27 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code here!
+const main = document.getElementById("main");
+main.remove();
+
+let newHeader = document.createElement('h1');
+document.body.appendChild(newHeader);
+newHeader.id = "victory";
+newHeader.innerHTML = 'Moumen is the champion';
