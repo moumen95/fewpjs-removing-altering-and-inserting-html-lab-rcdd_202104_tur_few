f90da02f5f6f334971bb0f825ddda5d0ee8d2849
Moumen Hallak <moumen95@gmail.com>
2021-04-23 14:02:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,4 +5,4 @@ main.remove();
 let newHeader = document.createElement('h1');
 document.body.appendChild(newHeader);
 newHeader.id = "victory";
-newHeader.innerHTML = 'Moumen';
+newHeader.innerHTML = 'Moumen is the champion';
