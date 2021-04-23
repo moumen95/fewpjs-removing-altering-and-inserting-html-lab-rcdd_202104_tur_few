2eea1618986eaa70bbf6478c3374f835e43273b3
Moumen Hallak <moumen95@gmail.com>
2021-04-23 13:54:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,5 +2,4 @@
 const main = document.getElementById("main");
 main.remove();
 
-let newHeader = document.getElementsByTagName('body');
-newHeader.innerHTML = '<h1 id="victory">H1</h1>';
+let newHeader = document.createElement('h1#victory');
